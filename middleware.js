3fa45de066616cb791b6c522ed987db12e089a9a
ShreyasPathe <90187847+ShreyasPathe@users.noreply.github.com>
2025-06-21 01:57:48 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -45,9 +45,9 @@ export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
-    "/dashboard(.*)",
-    "/account(.*)",
-    "/transaction(.*)",
-    "/api/(.*)",
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
   ],
 };
